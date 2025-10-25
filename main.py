45ad22e2167708c457e98759ca22dfb3777c00cc
Kelocena <kelocena@gmail.com>
2025-10-25 19:38:35 -0400

    got file opening and parsing correctly. windows stinky

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 
 ## build loca db? Would be nice to have a dictionary to index faster than iterating through the loca every time.
+import io
+from bs4 import BeautifulSoup
 
-def test():
-    print('h')
+def index_loca():
+    with io.open("resources/english.loca.xml", mode="r", encoding="utf-8") as eng:
+        # read_data = eng.read()
+        # print(read_data)
+        # thing = eng.decode('utf8')
+        print(eng)
+        # eng.decode('utf8')
+        loca = BeautifulSoup(eng, 'xml')
+        print(loca)
     
 
-test()
+index_loca()
