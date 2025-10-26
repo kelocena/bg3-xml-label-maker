0ca597199994c81eed34b27580fb9ab2c124a297
Kelocena <kelocena@gmail.com>
2025-10-25 20:25:55 -0400

    fx to write index to file and to read and find by handle

---
+++ label_maker.py
@@ -0,0 +1,5 @@
+import io
+import json
+from bs4 import BeautifulSoup
+
+# def class Label_Maker
--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 
 ## build loca db? Would be nice to have a dictionary to index faster than iterating through the loca every time.
 import io
+import json
 from bs4 import BeautifulSoup
 
-def index_loca():
+def build_loca_index():
+    loca_index = {}
     with io.open("resources/english.loca.xml", mode="r", encoding="utf-8") as eng:
-        # read_data = eng.read()
-        # print(read_data)
-        # thing = eng.decode('utf8')
-        print(eng)
-        # eng.decode('utf8')
-        loca = BeautifulSoup(eng, 'xml')
-        print(loca)
-    
 
-index_loca()
+        loca = BeautifulSoup(eng, 'xml')
+
+        print('indexing all handles')
+        for tag in loca.find_all("content"):
+            handle = tag['contentuid']
+            text = tag.string
+            loca_index[handle] = text
+    
+    with open("loca_index.txt",  mode="w", encoding="utf-8") as f:
+        f.write(json.dumps(loca_index))
+
+    print('loca file indexed and written to loca_index.txt as JSON')
+# build_loca_index()
+
+def test_read():
+    with io.open("loca_index.txt", mode="r", encoding="utf-8") as f:
+        # print('txt', f.read())
+        li = json.loads(f.read())
+        print(li['hffff8e50gcd5ag4d97gbc03gf783776542b4'])
+
+test_read()
